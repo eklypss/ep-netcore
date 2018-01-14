8850a740742900081d258daea5cf650fb3138ead
Alex <dwt416@gmail.com>
2018-01-15 00:44:16 +0200

    Set default limit to 100

--- ep-netcore/Helpers/Url/AvailableUrls.cs
+++ ep-netcore/Helpers/Url/AvailableUrls.cs
@@ -4,7 +4,7 @@
     {
         public const string BASE_URLS = "http://api.eliteprospects.com/beta/";
         public const string SEARCH_PLAYER_URL = "http://api.eliteprospects.com/beta/search?type=player&q={0}&apiKey={1}";
-        public const string PLAYER_STATS_URL = "http://api.eliteprospects.com/beta/players/{0}/stats?apiKey={1}";
+        public const string PLAYER_STATS_URL = "http://api.eliteprospects.com/beta/players/{0}/stats?limit=100&apiKey={1}";
         public const string SEARCH_TEAM_URL = "http://api.eliteprospects.com/beta/teams?filter=name={0}&apiKey={1}";
         public const string SEARCH_LEAGUE_URL = "http://api.eliteprospects.com/beta/leagues?filter=name={0}&apiKey={1}";
     }
